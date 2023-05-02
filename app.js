162e0cbf23588aa2be5a7989e0f7379b415e39cf
unknown <sebisonenblum@gmail.com>
2023-05-01 23:50:29 -0300

    creacion de las vistas

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-import ProductManager from './managers/ProductManager.js'
 import express from 'express'
 import __dirname from './utils.js'
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
+import viewsRouter from './routes/views.router.js'
+import { Server } from 'socket.io'
+import handlebars from 'express-handlebars';
 
 const app = express()
 
+// Public
+    app.use(express.static(`${__dirname}/public`))
 
+// Handlebars
+    app.engine('handlebars', handlebars.engine());
+    app.set('views', `${__dirname}/views`)
+    app.set('view engine', 'handlebars')
 
 // Configuracion de lectura de archivos y url
     app.use(express.json()) 
-        
     app.use(express.urlencoded({extended:true}))
         
 // Routers
     app.use('/api/carts', cartsRouter)
-        //Aca digo que cada vez que haya una peticion de esta url me derive a ese codigo,
-        // por lo que en users.router.js no pongo la ruta, ya se entiende
     app.use('/api/products', productsRouter)
+    app.use('/', viewsRouter)
 
 // Port
-    app.listen(8080, ()=>console.log("Listening on port 8080. Open http://localhost:8080"))
-
+    
+    const server = app.listen(8080, ()=>console.log("Listening on port 8080. Open http://localhost:8080"))
+    const io = new Server(server)
 
 
---
+++ routes/views.router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import ProductManager from "../managers/ProductManager.js";
+const router = Router()
+const productManager = new ProductManager('./files/products.json')
+
+router.get('/', async (req,res) =>{
+    const products = await productManager.getProducts()
+    // console.log(products)
+    res.render('home', {products, css:'home'})
+})
+router.get('/realTimeProducts', async (req,res)=>{
+    const products = await productManager.getProducts()
+    res.render('realTimeProducts', {products, css:'realTimeProducts'})
+})
+router.post('realTimeProducts',(req,res)=>{
+
+})
+export default router
